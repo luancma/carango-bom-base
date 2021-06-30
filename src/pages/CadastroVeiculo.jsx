b926bd1a051ba00e6e0f6b3b9a771a17d6f4fc0d
Andre Borges <andre.borges@thoughtworks.com>
2021-06-30 15:45:26 -0300

    Adicionada validação de campos da tela de cadastro de veículo

--- src/pages/CadastroVeiculo.jsx
+++ src/pages/CadastroVeiculo.jsx
@@ -1,4 +1,13 @@
-import { Button, makeStyles, TextField, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import {
+  Button,
+  makeStyles,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+} from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import useErros from "../hooks/useErros";
@@ -15,24 +24,44 @@ const useStyles = makeStyles(() => ({
 function CadastroVeiculo() {
   const [veiculo, setVeiculo] = useState(Veiculo.vazio());
   const [marcas, setMarcas] = useState([]);
+  const [noEstadoInicial, setNoEstadoInicial] = useState(true);
 
   const history = useHistory();
 
   const { id } = useParams();
 
   const classes = useStyles();
-  // ToDo: Implementar validações
-  //   const validacoes = {
-  //     veiculo: (dado) => {
-  //       if (dado && dado.length >= 3) {
-  //         return { valido: true };
-  //       } else {
-  //         return { valido: false, texto: "Veículo deve ter ao menos 3 letras." };
-  //       }
-  //     },
-  //   };
 
-  //   const [erros, validarCampos, possoEnviar] = useErros(validacoes);
+  const valorNaoEhVazio = (valor) => {
+    return !!valor
+      ? { valido: true, texto: "" }
+      : { valido: false, texto: "O campo não deve estar vazio." };
+  };
+
+  const valorNaoEhVazioEMaiorQueZero = (valor) => {
+    if (!!valor && valor > 0) {
+      return { valido: true, texto: "" };
+    } else {
+      return {
+        valido: false,
+        texto: "O campo deve estar preenchido com um valor maior que 0.",
+      };
+    }
+  };
+
+  const validacoes = {
+    marcaId: valorNaoEhVazio,
+    modelo: valorNaoEhVazio,
+    ano: valorNaoEhVazioEMaiorQueZero,
+    valor: valorNaoEhVazioEMaiorQueZero,
+  };
+
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes);
+
+  function validar(event) {
+    setNoEstadoInicial(false);
+    validarCampos(event);
+  }
 
   function cancelar() {
     history.goBack();
@@ -41,18 +70,17 @@ function CadastroVeiculo() {
   useEffect(() => {
     if (id) {
       VeiculoService.consultar(id).then((v) => setVeiculo(v));
+      setNoEstadoInicial(false);
     }
 
-    MarcaService.listar()
-      .then(marcas => setMarcas(marcas));
-
+    MarcaService.listar().then((marcas) => setMarcas(marcas));
   }, [id]);
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (/*possoEnviar()*/ true) {
+        if (possoEnviar()) {
           if (id) {
             VeiculoService.alterar(veiculo).then((res) => {
               history.goBack();
@@ -66,79 +94,91 @@ function CadastroVeiculo() {
         }
       }}
     >
-      <FormControl variant="outlined" fullWidth>
-        <InputLabel id="demo-simple-select-outlined-label">Marca</InputLabel>
+      <FormControl variant='outlined' fullWidth error={!erros.marcaId.valido}>
+        <InputLabel id='demo-simple-select-outlined-label'>Marca</InputLabel>
         <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
+          labelId='demo-simple-select-outlined-label'
+          id='demo-simple-select-outlined'
           value={veiculo.marcaId}
-          onChange={ (evt) => setVeiculo({ ...veiculo, marcaId: evt.target.value }) }
-          label="Marca"
+          onChange={(evt) =>
+            setVeiculo({ ...veiculo, marcaId: evt.target.value })
+          }
+          label='Marca'
+          name='marcaId'
           fullWidth
+          onBlur={validar}
         >
-          { marcas.map(marca => <MenuItem key={marca.id} value={marca.id}>{marca.nome}</MenuItem>) }
+          {marcas.map((marca) => (
+            <MenuItem key={marca.id} value={marca.id}>
+              {marca.nome}
+            </MenuItem>
+          ))}
         </Select>
+        {!erros.marcaId.valido && (
+          <FormHelperText>{erros.marcaId.texto}</FormHelperText>
+        )}
       </FormControl>
 
       <TextField
         value={veiculo.ano}
         onChange={(evt) => setVeiculo({ ...veiculo, ano: evt.target.value })}
-        // onBlur={validarCampos}
-        // helperText={erros.veiculo.texto}
-        // error={!erros.veiculo.valido}
-        name="ano"
-        id="ano"
-        label="Ano"
-        type="Number"
-        variant="outlined"
+        onBlur={validar}
+        helperText={erros.ano.texto}
+        error={!erros.ano.valido}
+        name='ano'
+        id='ano'
+        label='Ano'
+        type='Number'
+        min='0'
+        variant='outlined'
         fullWidth
         required
-        margin="normal"
+        margin='normal'
       />
       <TextField
         value={veiculo.modelo}
         onChange={(evt) => setVeiculo({ ...veiculo, modelo: evt.target.value })}
-        // onBlur={validarCampos}
-        // helperText={erros.veiculo.texto}
-        // error={!erros.veiculo.valido}
-        name="modelo"
-        id="modelo"
-        label="Modelo"
-        type="text"
-        variant="outlined"
+        onBlur={validar}
+        helperText={erros.modelo.texto}
+        error={!erros.modelo.valido}
+        name='modelo'
+        id='modelo'
+        label='Modelo'
+        type='text'
+        variant='outlined'
         fullWidth
         required
-        margin="normal"
+        margin='normal'
       />
       <TextField
         value={veiculo.valor}
         onChange={(evt) => setVeiculo({ ...veiculo, valor: evt.target.value })}
-        // onBlur={validarCampos}
-        // helperText={erros.veiculo.texto}
-        // error={!erros.veiculo.valido}
-        name="valor"
-        id="valor"
-        label="Valor"
-        type="Number"
-        variant="outlined"
+        onBlur={validar}
+        helperText={erros.valor.texto}
+        error={!erros.valor.valido}
+        name='valor'
+        id='valor'
+        label='Valor'
+        type='Number'
+        variant='outlined'
         fullWidth
         required
-        margin="normal"
+        margin='normal'
       />
 
       <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        // disabled={!possoEnviar()}
+        variant='contained'
+        color='primary'
+        type='submit'
+        disabled={noEstadoInicial || !possoEnviar()}
       >
         {id ? "Alterar" : "Cadastrar"}
       </Button>
 
       <Button
         className={classes.actions}
-        variant="contained"
-        color="secondary"
+        variant='contained'
+        color='secondary'
         onClick={cancelar}
       >
         Cancelar
