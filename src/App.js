96cc53d9063a7ffb2dd8e84e0f881679d7a0ccab
luan <luancmadev@gmail.com>
2021-07-06 15:10:51 -0300

    created the test config

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import { Container, CssBaseline, makeStyles } from '@material-ui/core';
-import blue from '@material-ui/core/colors/blue';
-import { ptBR } from '@material-ui/core/locale';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import CadastroMarca from './pages/CadastroMarca';
-import ListagemMarcas from './pages/ListagemMarcas';
+import { Container, CssBaseline, makeStyles } from "@material-ui/core";
+import blue from "@material-ui/core/colors/blue";
+import { ptBR } from "@material-ui/core/locale";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import CadastroMarca from "./pages/CadastroMarca";
+import ListagemMarcas from "./pages/ListagemMarcas";
 
-const muiTheme = createMuiTheme({
-  palette: {
-    primary: {
-      main: blue[900],
-    }
+const muiTheme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: blue[900],
+      },
+    },
   },
-}, ptBR);
+  ptBR
+);
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
+    color: "#fff",
   },
   content: {
     flexGrow: 1,
@@ -34,21 +37,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function App() {
-
   const classes = useStyles();
 
   return (
     <ThemeProvider theme={muiTheme}>
       <div className={classes.root}>
         <CssBaseline />
-        <main className={classes.content}>
+        <main data-testid="test" className={classes.content}>
           <div className={classes.toolbar} />
           <Container component="article" maxWidth="md">
             <Switch>
               <Route path="/cadastro-marca">
                 <CadastroMarca></CadastroMarca>
               </Route>
-              <Route path='/alteracao-marca/:id'>
+              <Route path="/alteracao-marca/:id">
                 <CadastroMarca></CadastroMarca>
               </Route>
               <Route path="/">
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,21 @@
-import { render } from "react-dom";
+import { screen, render } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+
+import App from "./App";
 
 describe("Testando aaa", () => {
   it("Shoud render the app component", () => {
-    render(<App />);
+    const history = createMemoryHistory();
 
-    expect(screen.getByRole("alert")).toHaveTextContent("alert");
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    const element = screen.getByTestId("test");
+
+    expect(element).toBeInTheDocument();
   });
 });
