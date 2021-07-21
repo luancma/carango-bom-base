4dbd751407ee602878236046b2b69f9ed854ffcc
Bruno Albuquerque <br.brunoalbuquerque@gmail.com>
2021-07-21 11:51:35 -0300

    Add confirm delete dialog

--- src/pages/ListVehicle.jsx
+++ src/pages/ListVehicle.jsx
@@ -6,40 +6,63 @@ import useVehicle from "hooks/useVehicle";
 
 import VehicleUtil from "util/VehicleUtil";
 
+import ConfirmDialog from "../components/ConfirmDialog";
+
 const ListVehicle = () => {
   const vehiclesPerPage = 10;
   const [defaultPage, setDefaultPage] = useState(0);
+  const [idToDelete, setIdToDelete] = useState("");
   const { vehicles, loading, fetchVehicles, vehiclesTotal, deleteVehicleById } =
     useVehicle({
       size: vehiclesPerPage,
     });
 
-  const handleItemClick = (id) => {
+  const [showConfirm, setShowConfirm] = useState(false);
+  const confirmProps = {
+    title: "Excluir",
+    message: "Tem certeza que deseja excluir?",
+  };
+
+  const handleItemClick = id => {
     console.log(`You clicked in vehicle with id ${id}`);
   };
 
-  const handleDeleteItem = async (evt, id) => {
-    evt.stopPropagation();
-    await deleteVehicleById(id);
+  const handleDeleteItem = async () => {
+    await deleteVehicleById(idToDelete);
     setDefaultPage(0);
+    setShowConfirm(false);
   };
 
-  const columns = VehicleUtil.getVehicleColumns({ onDelete: handleDeleteItem });
+  const handleOpenDialog = (evt, id) => {
+    evt.stopPropagation();
+    setIdToDelete(id);
+    setShowConfirm(true);
+  };
+
+  const columns = VehicleUtil.getVehicleColumns({ onDelete: handleOpenDialog });
 
   return (
-    <div style={{ height: 300, width: "100%" }}>
-      <DataGridPaginated
-        defaultPage={defaultPage}
-        loading={loading}
-        items={vehicles}
-        itemsPerPage={vehiclesPerPage}
-        totalItems={vehiclesTotal}
-        fetchItems={fetchVehicles}
-        onItemClick={handleItemClick}
-        onDeleteItem={handleDeleteItem}
-        columns={columns}
+    <>
+      <div style={{ height: 300, width: "100%" }}>
+        <DataGridPaginated
+          defaultPage={defaultPage}
+          loading={loading}
+          items={vehicles}
+          itemsPerPage={vehiclesPerPage}
+          totalItems={vehiclesTotal}
+          fetchItems={fetchVehicles}
+          onItemClick={handleItemClick}
+          columns={columns}
+        />
+      </div>
+      <ConfirmDialog
+        title={confirmProps.title}
+        message={confirmProps.message}
+        open={showConfirm}
+        onClose={() => setShowConfirm(false)}
+        onConfirm={() => handleDeleteItem()}
       />
-    </div>
+    </>
   );
 };
 
--- src/tests/pages/ListVehicle.test.js
+++ src/tests/pages/ListVehicle.test.js
@@ -1,11 +1,36 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
+import vehicleService from "services/VehicleService";
 
 import ListVehicle from "pages/ListVehicle";
 
+jest.mock("services/VehicleService");
+
 describe("ListVehicle component", () => {
+  beforeEach(() => {
+    vehicleService.getVehicles.mockResolvedValue({
+      content: [
+        {
+          model: "Yaris",
+          year: 2020,
+          price: 100000,
+          brand: {
+            name: "Toyota",
+          },
+          id: 1,
+        },
+      ],
+      total: 10,
+    });
+  });
   it("should render the list", () => {
     render(<ListVehicle />);
     expect(screen.getByRole("grid")).toBeInTheDocument();
+    expect(screen.getByText("Yaris")).toBeInTheDocument();
+  });
+  it("should show confirm delete dialog", () => {
+    render(<ListVehicle />);
+    fireEvent.click(screen.getByRole("button", { name: /remover/i }));
+    expect(screen.getByRole("dialog")).toBeInTheDocument();
   });
 });
